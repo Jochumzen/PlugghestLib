e6797e7c9c1c788ea033c7d9d1c647df5fb68e8c
Peter Jochumzen <peter.jochumzen@nek.lu.se>
2014-06-19 20:35:02 +0530

    1906_dayend_commit

--- Base2/UserControlRoot.cs
+++ Base2/UserControlRoot.cs
@@ -10,46 +10,95 @@ namespace Plugghest.Base2
     {
         public struct Common
         {
-            public const string OrderTitle = "~/USerControl/DisplayPlugg/Common/OrderTitle.ascx";
-            public const string AddNewComponent = "~/USerControl/DisplayPlugg/Common/AddNewComponent.ascx";
+            public const string OrderTitle = "~/CommonControls/DisplayPlugg/Common/OrderTitle.ascx";
+            public const string AddNewComponent = "~/CommonControls/DisplayPlugg/Common/AddNewComponent.ascx";
         }
 
         public struct RichRichText
         {
-            public const string Display = "~/USerControl/DisplayPlugg/RichRichText/Display.ascx";
-            public const string Edit1 = "~/USerControl/DisplayPlugg/RichRichText/Edit1.ascx";
-            public const string Edit2 = "~/USerControl/DisplayPlugg/RichRichText/Edit2.ascx";
-            public const string TranslationTitle = "~/USerControl/DisplayPlugg/RichRichText/TranslationTitle.ascx";
+            public const string Display = "~/CommonControls/DisplayPlugg/RichRichText/Display.ascx";
+            public const string Edit1 = "~/CommonControls/DisplayPlugg/RichRichText/Edit1.ascx";
+            public const string Edit2 = "~/CommonControls/DisplayPlugg/RichRichText/Edit2.ascx";
+            public const string TranslationTitle = "~/CommonControls/DisplayPlugg/RichRichText/TranslationTitle.ascx";
         }
 
         public struct RichText
         {
-            public const string Display = "~/USerControl/DisplayPlugg/RichText/Display.ascx";
-            public const string Edit1 = "~/USerControl/DisplayPlugg/RichText/Edit.ascx";
-            public const string Edit2 = "~/USerControl/DisplayPlugg/RichText/Edit2.ascx";
-            public const string TranslationTitle = "~/USerControl/DisplayPlugg/RichText/TranslationTitle.ascx";
+            public const string Display = "~/CommonControls/DisplayPlugg/RichText/Display.ascx";
+            public const string Edit1 = "~/CommonControls/DisplayPlugg/RichText/Edit.ascx";
+            public const string Edit2 = "~/CommonControls/DisplayPlugg/RichText/Edit2.ascx";
+            public const string TranslationTitle = "~/CommonControls/DisplayPlugg/RichText/TranslationTitle.ascx";
         }
 
         public struct Latex
         {
-            public const string Display = "~/USerControl/DisplayPlugg/Latex/Display.ascx";
-            public const string Edit1 = "~/USerControl/DisplayPlugg/Latex/Edit1.ascx";
-            public const string TranslationTitle = "~/USerControl/DisplayPlugg/Latex/TranslationTitle.ascx";
+            public const string Display = "~/CommonControls/DisplayPlugg/Latex/Display.ascx";
+            public const string Edit1 = "~/CommonControls/DisplayPlugg/Latex/Edit1.ascx";
+            public const string Edit2 = "~/CommonControls/DisplayPlugg/Latex/Edit2.ascx";
+            public const string TranslationTitle = "~/CommonControls/DisplayPlugg/Latex/TranslationTitle.ascx";
         }
 
         public struct Label
         {
-            public const string Display = "~/USerControl/DisplayPlugg/Label/Display.ascx";
-            public const string Edit1 = "~/USerControl/DisplayPlugg/Label/Edit1.ascx";
-            public const string Edit2 = "~/USerControl/DisplayPlugg/Label/Edit2.ascx";
-            public const string TranslationTitle = "~/USerControl/DisplayPlugg/Label/TranslationTitle.ascx";
+            public const string Display = "~/CommonControls/DisplayPlugg/Label/Display.ascx";
+            public const string Edit1 = "~/CommonControls/DisplayPlugg/Label/Edit1.ascx";
+            public const string Edit2 = "~/CommonControls/DisplayPlugg/Label/Edit2.ascx";
+            public const string TranslationTitle = "~/CommonControls/DisplayPlugg/Label/TranslationTitle.ascx";
         }
 
         public struct YouTube
         {
-            public const string Display = "~/USerControl/DisplayPlugg/YouTube/Display.ascx";
-            public const string Edit = "~/USerControl/DisplayPlugg/YouTube/Edit1.ascx";
-            public const string TranslationTitle = "~/USerControl/DisplayPlugg/YouTube/TranslationTitle.ascx";
+            public const string Display = "~/CommonControls/DisplayPlugg/YouTube/Display.ascx";
+            public const string Edit = "~/CommonControls/DisplayPlugg/YouTube/Edit1.ascx";
+            public const string TranslationTitle = "~/CommonControls/DisplayPlugg/YouTube/TranslationTitle.ascx";
+        }
+    }
+
+    public class UserControl_Resource_Root
+    {
+        public struct Common
+        {
+            public const string OrderTitle = "~/CommonControls/DisplayPlugg/Common/App_LocalResources/OrderTitle.ascx";
+            public const string AddNewComponent = "~/CommonControls/DisplayPlugg/Common/App_LocalResources/AddNewComponent.ascx";
+        }
+
+        public struct RichRichText
+        {
+            public const string Display = "~/CommonControls/DisplayPlugg/RichRichText/App_LocalResources/Display.ascx";
+            public const string Edit1 = "~/CommonControls/DisplayPlugg/RichRichText/App_LocalResources/Edit1.ascx";
+            public const string Edit2 = "~/CommonControls/DisplayPlugg/RichRichText/App_LocalResources/Edit2.ascx";
+            public const string TranslationTitle = "~/CommonControls/DisplayPlugg/RichRichText/App_LocalResources/TranslationTitle.ascx";
+        }
+
+        public struct RichText
+        {
+            public const string Display = "~/CommonControls/DisplayPlugg/RichText/App_LocalResources/Display.ascx";
+            public const string Edit1 = "~/CommonControls/DisplayPlugg/RichText/App_LocalResources/Edit.ascx";
+            public const string Edit2 = "~/CommonControls/DisplayPlugg/RichText/App_LocalResources/Edit2.ascx";
+            public const string TranslationTitle = "~/CommonControls/DisplayPlugg/RichText/App_LocalResources/TranslationTitle.ascx";
+        }
+
+        public struct Latex
+        {
+            public const string Display = "~/CommonControls/DisplayPlugg/Latex/App_LocalResources/Display.ascx";
+            public const string Edit1 = "~/CommonControls/DisplayPlugg/Latex/App_LocalResources/Edit1.ascx";
+            public const string Edit2 = "~/CommonControls/DisplayPlugg/Latex/App_LocalResources/Edit2.ascx";
+            public const string TranslationTitle = "~/CommonControls/DisplayPlugg/Latex/App_LocalResources/TranslationTitle.ascx";
+        }
+
+        public struct Label
+        {
+            public const string Display = "~/CommonControls/DisplayPlugg/Label/App_LocalResources/Display.ascx";
+            public const string Edit1 = "~/CommonControls/DisplayPlugg/Label/App_LocalResources/Edit1.ascx";
+            public const string Edit2 = "~/CommonControls/DisplayPlugg/Label/App_LocalResources/Edit2.ascx";
+            public const string TranslationTitle = "~/CommonControls/DisplayPlugg/Label/App_LocalResources/TranslationTitle.ascx";
+        }
+
+        public struct YouTube
+        {
+            public const string Display = "~/CommonControls/DisplayPlugg/YouTube/App_LocalResources/Display.ascx";
+            public const string Edit = "~/CommonControls/DisplayPlugg/YouTube/App_LocalResources/Edit1.ascx";
+            public const string TranslationTitle = "~/CommonControls/DisplayPlugg/YouTube/App_LocalResources/TranslationTitle.ascx";
         }
     }
 }
